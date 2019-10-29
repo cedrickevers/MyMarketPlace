2eb60e39306c7ad91bac9ff6b743c510f622953c
cedrickevers <keverscedric@gmail.com>
2019-10-29 01:59:57 +0100

    did add gig template and routes

--- routes/main.js
+++ routes/main.js
@@ -4,4 +4,9 @@ router.get('/', (req, res, next) => {
     res.render('main/home');
 });
 
+router.get("/my-gigs", (req, res, next) => {
+    res.render("main/my-gigs");
+
+});
+
 module.exports = router;
