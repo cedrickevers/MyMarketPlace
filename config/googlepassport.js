aeb04bcbda4a319b4150e6537345a6cdc1f22bc2
cedrickevers <keverscedric@gmail.com>
2019-10-30 00:32:28 +0100

    google auth, to do: secure data passsport

--- config/googlepassport.js
+++ config/googlepassport.js
@@ -4,8 +4,9 @@ const GoogleStrategy = require("passport-google-oauth20");
 
 passport.use( 
     new GoogleStrategy({
-
-        // options for the google strat
+                // options for the google 
+                clientID:"1097915437361-vprbtc7vo5t9437ccs8j6kg6rf36tjlj.apps.googleusercontent.com",
+                ClientSecret:"BSe4DHbMmSvhN1me72LfPoFe"
 
     }), () => {
         //passport callbkack option
